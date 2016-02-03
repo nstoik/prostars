6c95f0dfd1b70365f3dd2906e714a63481144514
Nelson Stoik <nelsonstoik@gmail.com>
2016-02-02 21:31:34 -0700

    added stats page

--- app/app.py
+++ app/app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 app.secret_key = 'mysecretkey'
 
 @app.route('/')
-def hello():
+def index():
 	return render_template('index.html')
+
+@app.route('/stats/')
+def stats():
+	return render_template('stats.html')
 
 if __name__ == "__main__":
 

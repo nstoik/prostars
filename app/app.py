df387107056e7f858da41439f6aed694c170e1fd
Nelson Stoik <nelsonstoik@gmail.com>
2016-02-09 00:27:58 -0700

    added ajax support

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, make_response
+from json import dumps
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
@@ -11,6 +12,17 @@ def index():
 @app.route('/stats/')
 def stats():
 	return render_template('stats.html')
+
+@app.route('/stats/load_default/', methods=['GET', 'POST'])
+def load_default():
+
+	sample_data = [
+		{'player': 'Nelson', 'gp': 1, 'g':1, 'a': 1, 'p': 2, 'pm': '+1', 'pim': 5, 'ppg': 2},
+		{'player': 'Nelson', 'gp': 1, 'g':1, 'a': 1, 'p': 2, 'pm': '+1', 'pim': 5, 'ppg': 2}
+		]
+
+	return jsonify(results = sample_data)
+	#return dumps(sample_data)
 
 if __name__ == "__main__":
 

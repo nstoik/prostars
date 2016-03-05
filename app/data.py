913b2a785b36c581838d165b673c779a3cd73295
Nelson Stoik <nelsonstoik@gmail.com>
2016-03-04 18:09:01 -0700

    fixed google API access

--- app/data.py
+++ app/data.py
@@ -5,7 +5,7 @@ from oauth2client.client import SignedJwtAssertionCredentials
 
 def connect():
 
-	json_key = json.load(open('app/Prostars-8c71dc0a18ea.json'))
+	json_key = json.load(open('app/Prostars-c02b661b2a77.json'))
 	scope = ['https://spreadsheets.google.com/feeds']
 	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
 	return gspread.authorize(credentials)

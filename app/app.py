a49e4e3aa6275c95fe292f0765feb51a1ed1b7a8
Nelson Stoik <nelson.stoik@gmail.com>
2017-02-28 20:50:14 -0700

    update for 2017 runtime

--- app/app.py
+++ app/app.py
@@ -1,40 +1,43 @@
 import os
 import sys
+from flask import Flask, render_template, jsonify, session, request
+
+from data import fetch_all
 
 sys.path.append(os.path.dirname(__file__))
-
-from flask import Flask, render_template, jsonify, session, request
-from data import fetch_all
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/stats/')
 def stats():
-	return render_template('hockey_stats.html')
+    return render_template('hockey_stats.html')
+
 
 @app.route('/stats/load_default/', methods=['GET', 'POST'])
 def load_default():
 
-	if request.method == 'POST':
-		table_id = request.form.get('table_id')
-		if (table_id == '#players-table'):
-			all_players = fetch_all('Hockey_Stats', 'Players')
-		elif (table_id == '#goalies-table'):
-			all_players = fetch_all('Hockey_Stats', 'Goalies')
-		
-		return jsonify(row_data = all_players, table_id = table_id)
+    if request.method == 'POST':
+        table_id = request.form.get('table_id')
+        if (table_id == '#players-table'):
+            all_players = fetch_all('Hockey_Stats', 'Players')
+        elif (table_id == '#goalies-table'):
+            all_players = fetch_all('Hockey_Stats', 'Goalies')
 
-	return
+        print(all_players)
+        print(table_id)
 
+        return jsonify(row_data=all_players, table_id=table_id)
+
+    return
 
 if __name__ == "__main__":
 
-	port = os.environ.get('PORT', 5000)
-	app.secret_key = 'mysecretkey'
-	app.run(host = '0.0.0.0', port = port)
-
+    port = os.environ.get('PORT', 5000)
+    app.secret_key = 'mysecretkey'
+    app.run(host='0.0.0.0', port=port, debug=True)
--- app/data.py
+++ app/data.py
@@ -5,48 +5,50 @@ from oauth2client.client import SignedJwtAssertionCredentials
 
 def connect():
 
-	json_key = json.load(open('app/Prostars-c02b661b2a77.json'))
-	scope = ['https://spreadsheets.google.com/feeds']
-	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
-	return gspread.authorize(credentials)
+    json_key = json.load(open('app/Prostars-c02b661b2a77.json'))
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+    return gspread.authorize(credentials)
+
 
 def fetch_all(spreadsheet_name, worksheet_name):
-	
-	gc = connect()
-	spreadsheet = gc.open(spreadsheet_name)
-	worksheet = spreadsheet.worksheet(worksheet_name)
 
-	all_data = worksheet.get_all_records(empty2zero = True)
+    gc = connect()
+    spreadsheet = gc.open(spreadsheet_name)
+    worksheet = spreadsheet.worksheet(worksheet_name)
 
-	return all_data
+    all_data = worksheet.get_all_records(empty2zero=True)
+
+    return all_data
+
 
 def fetch_filter_criteria_hockey(worksheet, pre_selected="true"):
 
-	#used for sorting days of week
-	days = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    # used for sorting days of week
+    days = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-	filter_criteria = []
-	for x in range(1,8):
-		#get all column data
-		column = worksheet.col_values(x)
-		#remove column name
-		column_name = column.pop(0)
+    filter_criteria = []
+    for x in range(1, 8):
+        # get all column data
+        column = worksheet.col_values(x)
+        # remove column name
+        column_name = column.pop(0)
 
-		#get only unique values. Sort days of week special
-		if column_name == "Night":
-			unique_column = sorted(set(column), key=days.index)
-		else:
-			unique_column = sorted(set(column))
+        # get only unique values. Sort days of week special
+        if column_name == "Night":
+            unique_column = sorted(set(column), key=days.index)
+        else:
+            unique_column = sorted(set(column))
 
-		filter_data = [column_name]
-		for item in unique_column:
-			select_data = {}
-			select_data['label'] = item
-			select_data['title'] = item
-			select_data['value'] = item
-			select_data['selected'] = pre_selected
-			filter_data.append(select_data)
+        filter_data = [column_name]
+        for item in unique_column:
+            select_data = {}
+            select_data['label'] = item
+            select_data['title'] = item
+            select_data['value'] = item
+            select_data['selected'] = pre_selected
+            filter_data.append(select_data)
 
-		filter_criteria.append(filter_data)
+        filter_criteria.append(filter_data)
 
-	return filter_criteria
+    return filter_criteria

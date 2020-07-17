069e74536ba6c58ea58c7d80df99ea7d6e6ca23e
nstoik <nelsonstoik@gmail.com>
2020-07-16 20:42:04 -0600

    Update data.py
    update to work with new version of  gspread. Remove oauth2client (deprecated by Google API)

--- app/data.py
+++ app/data.py
@@ -1,15 +1,12 @@
 import json
 import gspread
-from oauth2client.client import SignedJwtAssertionCredentials
 
 
 def connect():
 
-    json_key = json.load(open('app/Prostars-c02b661b2a77.json'))
-    scope = ['https://spreadsheets.google.com/feeds']
-    credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
-    return gspread.authorize(credentials)
-
+	gc = gspread.service_account(filename='app/Prostars-c02b661b2a77.json')
+	
+	return gc
 
 def fetch_all(spreadsheet_name, worksheet_name):
 

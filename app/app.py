bd570f1c05ffca1b9ca533f102f0c3d2febbd24d
nstoik <nelsonstoik@gmail.com>
2020-07-23 18:59:17 -0600

    get baseball workin
    Working version of baseball stats for players only

--- app/app.py
+++ app/app.py
@@ -14,48 +14,29 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/stats/hockey/')
-def hockey_stats():
-    return render_template('hockey_stats.html')
+@app.route('/stats/<sport>/')
+def sport_stats(sport:str):
+    return render_template(f'{sport}_stats.html', sport=sport)
 
 
-@app.route('/stats/hockey/load_default/', methods=['GET', 'POST'])
-def load_default_hockey():
-
-    if request.method == 'POST':
-        table_id = request.form.get('table_id')
-        if (table_id == '#hockey-players-table'):
-            all_players = fetch_all('Hockey_Stats', 'Players')
-        elif (table_id == '#hockey-goalies-table'):
-            all_players = fetch_all('Hockey_Stats', 'Goalies')
-
-        print(all_players)
-        print(table_id)
-
-        return jsonify(row_data=all_players, table_id=table_id)
-
-    	
-@app.route('/stats/baseball/')
-def baseball_stats():
-    return render_template('baseball_stats.html')
-
-
-@app.route('/stats/baseball/load_default/', methods=['GET', 'POST'])
-def load_default_baseball():
+@app.route('/stats/<sport>/load_default/', methods=['GET', 'POST'])
+def load_default_sport(sport:str):
 
     if request.method == 'POST':
         table_id = request.form.get('table_id')
-        if (table_id == '#baseball-players-table'):
-            all_players = fetch_all('Baseball_Stats', 'Players')
-        elif (table_id == '#baseball-pitchers-table'):
-            all_players = fetch_all('Baseball_Stats', 'Pitchers')
-
-        print(all_players)
-        print(table_id)
+        if sport == 'hockey':
+            if (table_id == '#hockey-players-table'):
+                all_players = fetch_all('Hockey_Stats', 'Players')
+            elif (table_id == '#hockey-goalies-table'):
+                all_players = fetch_all('Hockey_Stats', 'Goalies')
+        elif sport == 'baseball':
+            if (table_id == '#baseball-players-table'):
+                all_players = fetch_all('dev_baseball_stats', 'Master_Batting')
+            elif (table_id == '#baseball-pitchers-table'):
+                all_players = fetch_all('dev_baseball_stats', 'Pitchers')
 
         return jsonify(row_data=all_players, table_id=table_id)
 
-    return
 
 if __name__ == "__main__":
 

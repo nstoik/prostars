7a9b4cbe2a466666d71e7ecfda4a6f12f838836c
Nelson Stoik <nelsonstoik@gmail.com>
2022-11-21 23:06:05 -0700

    run black

--- prostars/app.py
+++ prostars/app.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from flask import Flask, render_template, jsonify, session, request, send_file
+from flask import Flask, render_template, jsonify, request
 
 from prostars.data import fetch_all
 
@@ -9,38 +9,46 @@ sys.path.append(os.path.dirname(__file__))
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/stats/<sport>/')
-def sport_stats(sport:str):
-    return render_template(f'{sport}_stats.html', sport=sport)
+@app.route("/stats/<sport>/")
+def sport_stats(sport: str):
+    return render_template(f"{sport}_stats.html", sport=sport)
 
 
-@app.route('/stats/<sport>/load_default/', methods=['GET', 'POST'])
-def load_default_sport(sport:str):
+@app.route("/stats/<sport>/load_default/", methods=["GET", "POST"])
+def load_default_sport(sport: str):
 
-    if request.method == 'POST':
-        table_id = request.form.get('table_id')
-        if sport == 'hockey':
-            if (table_id == '#hockey-players-table'):
-                all_players = fetch_all('Hockey_Stats', 'Players')
-            elif (table_id == '#hockey-goalies-table'):
-                all_players = fetch_all('Hockey_Stats', 'Goalies')
-        elif sport == 'baseball':
-            if (table_id == '#baseball-players-table'):
-                treat_columns_as_strings=[19,20,21,22]
-                all_players = fetch_all('baseball_stats', 'Master_Batting',numericise_ignore=treat_columns_as_strings)
-            elif (table_id == '#baseball-pitchers-table'):
-                treat_columns_as_strings=[10, 15,16,17]
-                all_players = fetch_all('baseball_stats', 'Master_Pitching',numericise_ignore=treat_columns_as_strings)
+    if request.method == "POST":
+        table_id = request.form.get("table_id")
+        if sport == "hockey":
+            if table_id == "#hockey-players-table":
+                all_players = fetch_all("Hockey_Stats", "Players")
+            elif table_id == "#hockey-goalies-table":
+                all_players = fetch_all("Hockey_Stats", "Goalies")
+        elif sport == "baseball":
+            if table_id == "#baseball-players-table":
+                treat_columns_as_strings = [19, 20, 21, 22]
+                all_players = fetch_all(
+                    "baseball_stats",
+                    "Master_Batting",
+                    numericise_ignore=treat_columns_as_strings,
+                )
+            elif table_id == "#baseball-pitchers-table":
+                treat_columns_as_strings = [10, 15, 16, 17]
+                all_players = fetch_all(
+                    "baseball_stats",
+                    "Master_Pitching",
+                    numericise_ignore=treat_columns_as_strings,
+                )
 
         return jsonify(row_data=all_players, table_id=table_id)
 
 if __name__ == "__main__":
 
-    port = os.environ.get('PORT', 5000)
-    app.secret_key = 'mysecretkey'
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = os.environ.get("PORT", 5000)
+    app.secret_key = "mysecretkey"
+    app.run(host="0.0.0.0", port=port, debug=True, reload=True)
